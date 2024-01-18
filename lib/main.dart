7c821c90e8bcc5298524cf818c77821332d634ae
ORG-www <sharmachaitanyaind@gmail.com>
2024-01-18 17:26:15 +0530

    assignment 5 done with setstate

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,95 @@
+
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  Color backgroundColor = Colors.blueGrey;
-
-  void changeColor(Color color) {
-    setState(() {
-      backgroundColor = color;
-    });
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          backgroundColor: backgroundColor,
-
-        body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: () => changeColor(Colors.white),
-              child: const Text('Customer'),
-            ),
-            ElevatedButton(
-              onPressed: () => changeColor(Colors.redAccent),
-              child: const Text('Agent'),
-            ),
-            ElevatedButton(
-              onPressed: () => changeColor(Colors.green),
-              child: const Text('Merchant'),
-            ),
-          ],
-        ),
-      ),
-      ),
+    return const MaterialApp(
+      home: CheckBox(),
     );
   }
 }
 
+class CheckBox extends StatefulWidget {
+
+  const CheckBox({Key? key}) : super(key: key);
+
+  @override
+  State<CheckBox> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<CheckBox> {
+
+  final StreamController<bool> _checkBoxController = StreamController();
+  Stream<bool> get _checkBoxStream => _checkBoxController.stream;
+
+  @override
+  void dispose() {
+    _checkBoxController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Checkbox with stream builder"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Name",
+              ),
+            ),
+            const SizedBox(height: 10,),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Phone",
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text("Accept"),
+                StreamBuilder(
+                    stream: _checkBoxStream,
+                    initialData: false,
+                    builder: (BuildContext context, AsyncSnapshot<bool> snapshot ){
+                      return Checkbox(
+                          value: snapshot.data,
+                          onChanged: (changedValue){
+                            _checkBoxController.sink.add(changedValue!);
+                          }
+                      );
+                    }),
+              ],
+            ),
+            const SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){},
+                child: const Text("Submit"))
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 
